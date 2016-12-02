ba75dfd67d95f14c0987a0a05cb4074e176d9d23
Getro Naissance <getro89@gmail.com>
2016-12-02 09:20:24 -0800

    add 6 rspec tests while writing the method in order to make them pass

---
+++ lib/words.rb
@@ -0,0 +1,35 @@
+class Words
+  @@words_list = []
+  attr_reader(:word)
+
+  define_method(:initialize) do |attributes|
+    @word = attributes.fetch(:word)
+    @id = @@words_list.length().+(1)
+  end
+
+  define_singleton_method(:all) do
+    @@words_list
+  end
+
+  define_method(:save) do
+    @@words_list.push(self)
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@words_list.each() do |word|
+      if word.id() == id.to_i
+        found_word = word
+      end
+    end
+    found_word
+  end
+
+  define_singleton_method(:clear) do
+    @@Words_list = []
+  end
+end
---
+++ spec/words_spec.rb
@@ -0,0 +1,51 @@
+require('rspec')
+require('definitions')
+require('words')
+
+describe(Words) do
+
+  before() do
+    Words.clear()
+  end
+
+  describe('#word') do
+    it('returns the word created') do
+      test_word = Words.new({:word => "Getro"})
+      expect(test_word.word()).to(eq("Getro"))
+    end
+  end
+
+  describe('.all') do
+    it('returns an empty array at first') do
+      test_word = Words.new({:word => "Getro"})
+      expect(Words.all()).to(eq([]))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id of the word create') do
+       test_word = Words.new({:word => "Getro"})
+      expect(test_word.id()).to(eq(1))
+    end
+  end
+
+  describe('#save') do
+    it('saves the new word that was created') do
+      test_word = Words.new({:word => "Getro"})
+      test_word.save()
+      expect(Words.all()).to(eq([test_word]))
+    end
+  end
+
+  describe('.find') do
+    it('will find the word in question with the help of the id') do
+      test_word = Words.new({:word => "Getro"})
+      test_word.save()
+      test_word2 = Words.new({:word => "Getro"})
+      test_word2.save()
+      expect(Words.find(test_word.id())).to(eq(test_word))
+    end
+  end
+
+
+end
