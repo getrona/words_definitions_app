89f7988929d701be089086077d0a4f0e2dafc2bb
Getro Naissance <getro89@gmail.com>
2016-12-02 12:31:51 -0800

    add styling, jumbotron, wells along with logic for the app, from index to the form

--- app.rb
+++ app.rb
@@ -7,11 +7,18 @@ get('/') do
   erb(:index)
 end
 
-get('/words/form') do
+get('/words/new/form') do
   erb(:words_form)
 end
 
 post('/words') do
-  
+  new_word = params.fetch('word_input')
+  create_word = Words.new(:word => new_word)
+  create_word.save()
   erb(:success)
+end
+
+get('/words') do
+  @words_list = Words.all()
+  erb(:index)
 end
